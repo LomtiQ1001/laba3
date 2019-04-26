f825b0c32b5e6aabc0c5be56df6ebbcf54e0e415
LomtiQ1001 <47700444+LomtiQ1001@users.noreply.github.com>
2019-04-26 17:03:04 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -5,32 +5,32 @@
 using namespace std;
 
 void Vector::set_x(int vector_x){
-    Vector::x = vector_x;
+    x = vector_x;
 }
 int Vector::get_x(){
-    return Vector::x;
+    return x;
 }
 
 
 void Vector::set_y(int vector_y){
-    Vector::y = vector_y;
+    y = vector_y;
 }
 int Vector::get_y(){
-    return Vector::y;
+    return y;
 }
 
 
 void Vector::set_z(int vector_z){
-   Vector::z = vector_z;
+   z = vector_z;
 }
 int Vector::get_z(){
-    return Vector::z;
+    return z;
 }
 
 
 int Vector::modul(){
-    Vector::mod = sqrt((x*x) + (y*y) + (z*z));
-    return Vector::mod;
+    mod = sqrt((x*x) + (y*y) + (z*z));
+    return mod;
 }
 
 
